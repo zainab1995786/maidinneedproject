15a0baae0df4197e44a8ce63db9804ad95730e0e
zainab1995786 <zainabjawad757@gmail.com>
2021-03-15 15:31:18 -0700

    BookAService And Date And Time Done

---
+++ app/src/main/java/com/example/maidinneed/BookAService.java
@@ -0,0 +1,14 @@
+package com.example.maidinneed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookAService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_aservice);
+    }
+}
