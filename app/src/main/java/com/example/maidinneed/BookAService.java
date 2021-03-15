e75413a2fb9cf912916111f9e0b8dd1945d5efa2
zainab1995786 <zainabjawad757@gmail.com>
2021-03-15 16:08:05 -0700

    BookAService, Date And Time And Thankyou Page Done

--- app/src/main/java/com/example/maidinneed/BookAService.java
+++ app/src/main/java/com/example/maidinneed/BookAService.java
@@ -2,7 +2,9 @@ package com.example.maidinneed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BookAService extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class BookAService extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_aservice);
+
+    }
+
+    public void servicebooknextbtn(View view) {
+
+        Intent bookservice = new Intent(BookAService.this,DateAndTime.class);
+        startActivity(bookservice);
     }
 }
--- app/src/main/java/com/example/maidinneed/DateAndTime.java
+++ app/src/main/java/com/example/maidinneed/DateAndTime.java
@@ -2,7 +2,9 @@ package com.example.maidinneed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DateAndTime extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class DateAndTime extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_and_time);
+
+    }
+
+    public void next(View view) {
+        Intent thankyou = new Intent(DateAndTime.this,ThankYou.class);
+        startActivity(thankyou);
     }
 }
